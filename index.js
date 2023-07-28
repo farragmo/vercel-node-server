6b8dc5c267903ff2d29b3a75504c8791486e604e
mostafa farrag <adam.mostafa@outlook.de>
2023-07-28 21:49:49 +0200

    update and add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cors = require('cors')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 const express = require("express");
 const app = express();
 
@@ -8,7 +9,19 @@ const port = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
+// mongoose
+mongoose.connect(`${process.env.MONGO_URL}`)
+    .then(th => {
+        console.log('db connected')
+    }).catch(error => console.log('db error:', error.message))
 
+// schema
+const schema = new mongoose.Schema({
+    email: { type: String }
+})
+const Model$ = mongoose.model('User', schema)
+
+// routes
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
@@ -21,6 +34,11 @@ app.get("/api/v0", (req, res) => {
     res.send("start api version zero ...");
 });
 
+//
+app.post('/api/v0/user', async (req, res) => {
+    const user = await (await Model$.create(req.body)).save()
+    req.send(user)
+})
 
 // Initialize server
 app.listen(port, () => {
